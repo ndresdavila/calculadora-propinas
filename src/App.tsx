e5c496cc13874b0d3f96606e2510615699d9ee06
Andres Davila <andresdavilasanchez@gmail.com>
2024-10-29 13:15:26 -0500

    Header and menu items added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import MenuItem from "./components/MenuItem";
+import { menuItems } from "./data/db";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl">
+          Calculadora de Propinas y Consumo
+        </h1>
+      </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menu</h2>
+
+          {menuItems.map((item) => (
+            <MenuItem key={item.id} item={item} />
+          ))}
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // le especificamos en donde habra archivos html con clases css
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
